8f9ec74f4fc592a84c4e2827fb4188ed1606940f
wcreecy <william.creecy@gmail.com>
2025-12-23 10:23:53 -0500

    Removed unused files:

--- SharedJournalViews.swift
+++
@@ -1,8 +0,0 @@
-// Intentionally empty. Components were split into:
-// - JournalNotifications.swift
-// - ScriptureRefExtractor.swift
-// - ScripturePreviewCard.swift
-// - ScriptureLinksList.swift
-// - TagChip.swift
-// - TagChipRow.swift
-// - CursorTextView.swift
--- The Bible (iOS)/GoToPickerView.swift
+++
@@ -1,1 +0,0 @@
-// GoToPickerView has been removed as part of reverting Option 2 (Reader-first Go To sheet).
